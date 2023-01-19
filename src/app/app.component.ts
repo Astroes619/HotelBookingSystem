3b5cbef75f5a313890435af59420de1b7905531b
Atheeque Hasan <aiaahasan@students.nsbm.ac.lk>
2023-01-19 13:01:23 +0530

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  search() {
+    throw new Error('Method not implemented.');
+  }
+  selectHotel(_t9: any) {
+    throw new Error('Method not implemented.');
+  }
   title = 'HotelBookingSystem';
+  searchTerm: any;
+  searchResults: any;
 }
